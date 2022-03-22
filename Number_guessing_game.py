3ef12bfbdeaae36cb95e056f5bf3403fce0e16cd
Miguel Santacruz <miguel.santacruz@mths.ca>
2022-03-22 17:59:59 +0000

    number guessign game

---
+++ Number_guessing_game.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Miguel Santacruz
+# Created on: Mar 2022
+# This program is a number guessing game
+
+import constants
+
+
+def main():
+    # This function checks the number
+
+    # input
+    number = int(input("Enter number between 0 - 9: "))
+
+    # process & output
+    print("")
+    if number == constants.right_number:
+        print("You guessed right!")
+    if number != constants.right_number:
+        print("You guessed wrong!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by: Miguel Santacruz
+# Created on: Mar 2022
+# This program states constants for number_guessing_game
+
+right_number = 7
